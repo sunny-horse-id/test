6def83e41a0750687afc611896627f2d0b4e01f2
zy <1824762582@qq.com>
2024-03-09 13:26:39 +0800

    ElementPlus主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 //ElementPlus按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [
+                // 1. 配置elementPlus采用sass样式配色系统
+                ElementPlusResolver({importStyle: "sass"}),
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 2. 自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+                  @use "@/styles/element/index.scss" as *;
+                `,
+            }
+        }
     }
-  }
 })
