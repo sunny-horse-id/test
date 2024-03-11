1e4eb05f914ff22284246648e39e8ccf09b7fcca
zy <1824762582@qq.com>
2024-03-11 15:00:52 +0800

    路由的修复

--- src/router/index.js
+++ src/router/index.js
@@ -10,24 +10,24 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/login',
-            component: Login,
+            path: '/',
+            component: Layout,
             children: [
                 {
-                    path:'',
+                    path: '',
                     component: Home
                 },
                 {
-                  path: 'category',
-                  component: Category
-                }
+                    path: 'category',
+                    component: Category
+                },
             ]
         },
         {
-            path: '/',
-            component: Layout
+            path: '/login',
+            component: Login
         }
-    ]
+    ],
 })
 
 export default router
