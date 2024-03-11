d63baabf544c958de6774ee2e56385a25a415f74
zy <1824762582@qq.com>
2024-03-11 14:36:51 +0800

    pinia优化重复请求

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -1,46 +1,20 @@
 <script setup>
 import { useScroll } from '@vueuse/core'
 const {y} = useScroll(window)
-</script>
 
+//使用pinia中的数据
+import {useCategoryStore} from "@/stores/category.js";
+const categoryStore = useCategoryStore()
+</script>
 <template>
   <div class="app-header-sticky" :class="{show: y > 78}">
     <div class="container">
-      <RouterLink class="logo" to="/" />
-      <!-- 导航区域 -->
-      <ul class="app-header-nav ">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">居家</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">美食</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">服饰</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">母婴</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">个护</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">严选</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">数码</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">运动</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">杂项</RouterLink>
+      <RouterLink class="logo" to = '/' />
+      <ul class="app-header-nav">
+        <li class="home" v-for="item in categoryStore.categoryList" :key = "item.id">
+          <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
       </ul>
-
       <div class="right">
         <RouterLink to="/">品牌</RouterLink>
         <RouterLink to="/">专题</RouterLink>
